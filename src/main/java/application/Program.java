7d36084cfa0b5d62fb0b4cfe4893d060cc12b50f
antoniovsegala <antoniosegala023@gmail.com>
2025-06-22 17:33:42 -0300

    Software que cálcula o valor a se pagar para trocar reais por dólares. Aprendi com esse software a manusear os membros estáticos.

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import util.Calculator;
+
+import java .util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What is the dollar price?");
+        double priceDollar = sc.nextDouble();
+
+        System.out.println("How many dollars will be bought? ");
+        double value = sc.nextDouble();
+
+        double c = Calculator.currencyConverter(priceDollar, value);
+
+        System.out.printf("Amount to be paid in reais : %.2f%n", c);
+
+
+        sc.close();
+    }
+
+}
---
+++ src/main/java/util/Calculator.java
@@ -0,0 +1,9 @@
+package util;
+
+public class Calculator {
+
+    public static double currencyConverter(double priceDollar, double value) {
+        return priceDollar * value * 1.06;
+    }
+
+}
